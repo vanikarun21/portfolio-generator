2656cee5a582a5b0e2dd20afbd74d00dc7c74395
Vani Karun <vanikarun21@yahoo.com>
2022-04-27 15:30:55 -0500

    initial files

---
+++ app.js
@@ -0,0 +1,27 @@
+// const animalArray = ['dog', 'cat', 'pig'];
+
+// animalArray.push('cow');
+
+// const personObj = {
+//   name: 'Lernantino',
+//   age: 99
+// };
+
+// personObj.age = 100;
+// personObj.occupation = 'Developer';
+
+
+
+
+
+
+
+
+
+
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+  };
+  
+  printProfileData(profileDataArgs);
+  
