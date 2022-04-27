15c372d0c67eb3b8f11dab5eaa6c5a7bb3e05eaa
Vani Karun <vanikarun21@yahoo.com>
2022-04-27 15:43:24 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-// const animalArray = ['dog', 'cat', 'pig'];
-
-// animalArray.push('cow');
-
-// const personObj = {
-//   name: 'Lernantino',
-//   age: 99
-// };
-
-// personObj.age = 100;
-// personObj.occupation = 'Developer';
-
-
-
-
-
-
-
-
-
 
 const profileDataArgs = process.argv.slice(2);
 
