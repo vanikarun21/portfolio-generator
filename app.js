dcf4e2a4b3a7713d85102469b1d7167560735a9c
Vani Karun <vanikarun21@yahoo.com>
2022-04-27 15:35:37 -0500

    updated codes from snapshot

--- app.js
+++ app.js
@@ -19,9 +19,19 @@
 
 
 
-const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-  };
-  
-  printProfileData(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
   
